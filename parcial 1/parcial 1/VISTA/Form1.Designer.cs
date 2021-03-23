3dbeef67742186e6a4cd80bfdebabb5b3edcb8b2
kevin alexander <no0336032020@unab.edu.sv>
2021-03-23 12:33:30 -0600

    actualizacion

--- parcial 1/parcial 1/VISTA/Form1.Designer.cs
+++ parcial 1/parcial 1/VISTA/Form1.Designer.cs
@@ -29,13 +29,211 @@ namespace parcial_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblId_empleado = new System.Windows.Forms.Label();
+            this.txtId_empleado = new System.Windows.Forms.TextBox();
+            this.lblEmpl_nombre = new System.Windows.Forms.Label();
+            this.txtEmpl_nombre = new System.Windows.Forms.TextBox();
+            this.lblEmpl_apellido = new System.Windows.Forms.Label();
+            this.txtEmpl_apellido = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId_empleado
+            // 
+            this.lblId_empleado.AutoSize = true;
+            this.lblId_empleado.Location = new System.Drawing.Point(58, 40);
+            this.lblId_empleado.Name = "lblId_empleado";
+            this.lblId_empleado.Size = new System.Drawing.Size(68, 13);
+            this.lblId_empleado.TabIndex = 0;
+            this.lblId_empleado.Text = "Id_empleado";
+            // 
+            // txtId_empleado
+            // 
+            this.txtId_empleado.Location = new System.Drawing.Point(61, 70);
+            this.txtId_empleado.Name = "txtId_empleado";
+            this.txtId_empleado.Size = new System.Drawing.Size(138, 20);
+            this.txtId_empleado.TabIndex = 1;
+            // 
+            // lblEmpl_nombre
+            // 
+            this.lblEmpl_nombre.AutoSize = true;
+            this.lblEmpl_nombre.Location = new System.Drawing.Point(58, 128);
+            this.lblEmpl_nombre.Name = "lblEmpl_nombre";
+            this.lblEmpl_nombre.Size = new System.Drawing.Size(71, 13);
+            this.lblEmpl_nombre.TabIndex = 2;
+            this.lblEmpl_nombre.Text = "Empl_nombre";
+            // 
+            // txtEmpl_nombre
+            // 
+            this.txtEmpl_nombre.Location = new System.Drawing.Point(61, 174);
+            this.txtEmpl_nombre.Name = "txtEmpl_nombre";
+            this.txtEmpl_nombre.Size = new System.Drawing.Size(138, 20);
+            this.txtEmpl_nombre.TabIndex = 3;
+            // 
+            // lblEmpl_apellido
+            // 
+            this.lblEmpl_apellido.AutoSize = true;
+            this.lblEmpl_apellido.Location = new System.Drawing.Point(58, 240);
+            this.lblEmpl_apellido.Name = "lblEmpl_apellido";
+            this.lblEmpl_apellido.Size = new System.Drawing.Size(72, 13);
+            this.lblEmpl_apellido.TabIndex = 4;
+            this.lblEmpl_apellido.Text = "Empl_apellido";
+            // 
+            // txtEmpl_apellido
+            // 
+            this.txtEmpl_apellido.Location = new System.Drawing.Point(61, 285);
+            this.txtEmpl_apellido.Name = "txtEmpl_apellido";
+            this.txtEmpl_apellido.Size = new System.Drawing.Size(138, 20);
+            this.txtEmpl_apellido.TabIndex = 5;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(337, 40);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(55, 13);
+            this.lbl.TabIndex = 6;
+            this.lbl.Text = "Empl_DUI";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(331, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Empl_direccion";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(331, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Empl_tel";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(331, 285);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(626, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Empl_email";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(608, 70);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(138, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(626, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Empl_cargo";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(608, 174);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(138, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(61, 341);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 16;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 492);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.txtEmpl_apellido);
+            this.Controls.Add(this.lblEmpl_apellido);
+            this.Controls.Add(this.txtEmpl_nombre);
+            this.Controls.Add(this.lblEmpl_nombre);
+            this.Controls.Add(this.txtId_empleado);
+            this.Controls.Add(this.lblId_empleado);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblId_empleado;
+        private System.Windows.Forms.TextBox txtId_empleado;
+        private System.Windows.Forms.Label lblEmpl_nombre;
+        private System.Windows.Forms.TextBox txtEmpl_nombre;
+        private System.Windows.Forms.Label lblEmpl_apellido;
+        private System.Windows.Forms.TextBox txtEmpl_apellido;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
 
--- parcial 1/parcial 1/VISTA/Form1.cs
+++ parcial 1/parcial 1/VISTA/Form1.cs
@@ -16,5 +16,10 @@ namespace parcial_1
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
